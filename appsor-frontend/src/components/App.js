46f40915c7942219e9544bc79294b2ab3bd500ee
Adel Mandanas <mandanas.adel@gmail.com>
2018-06-20 20:18:58 +0800

    Add linting, Create basic structure of the project

---
+++ appsor-frontend/src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  component() {
+    this.state = {
+      sample: true,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <img alt="logo" />
+          <h1>Welcome to React</h1>
+        </header>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
