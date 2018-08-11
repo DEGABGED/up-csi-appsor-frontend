2d4f4d19874ee43ea602fb706b9e1183a9d8bc0a
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-08-11 14:42:30 +0800

    Add committee seeds and handleSubmit for form

--- appsor-backend/db/seeds.rb
+++ appsor-backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Committee.delete_all
+[
+  {name: 'Engineering', id: 1, desc: 'engg'},
+  {name: 'Innovations', id: 2, desc: 'innov'},
+  {name: 'Publicity', id: 3, desc: 'pub'},
+  {name: 'Membership and Internal', id: 4, desc: 'mem'},
+  {name: 'External Relations', id: 5, desc: 'exte'},
+  {name: 'Service', id: 6, desc: 'serv'},
+].each do |c|
+  Committee.create!(c)
+end
