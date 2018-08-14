d8c0416a9dbee60957d7ab0e88fc4988f3e7422a
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-08-14 23:53:06 +0800

    Add navbar

--- appsor-frontend/src/components/App.js
+++ appsor-frontend/src/components/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Image } from 'semantic-ui-react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './container/pages/Home';
 import Result from './container/pages/Result';
 import AppFormik from './AppFormik';
+import upcsi from '../assets/images/upcsi.png';
 import '../assets/stylesheets/App.css';
 
 const NavBar = () => (
-  <ul>
-    <li>
-      <Link to="/committees">Committees</Link>
-    </li>
-    <li>
-      <Link to="/form">Form</Link>
-    </li>
-  </ul>
+  <div id="navbar">
+    <Image className="navbar-logo" src={upcsi} size="mini" />
+    <ul className="navbar-item-list">
+      <li className="navbar-item">
+        <Link to="/committees">Committees</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/form">Form</Link>
+      </li>
+    </ul>
+  </div>
 );
 
 const App = ({ values }) => (
