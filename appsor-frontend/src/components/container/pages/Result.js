0ec9137b8ee0ff76b0b9bd2d18d7be8e54083ff6
Jonathan Cancio <jonahcancio@gmail.com>
2018-08-26 17:57:52 +0800

    Added MobileCarousel and Responsiveness

--- appsor-frontend/src/components/container/pages/Result.js
+++ appsor-frontend/src/components/container/pages/Result.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
-import CommCarousel from '../../presentational/design/CommCarousel'
+import CommCarousel from '../../presentational/design/DesktopCarousel'
+import MobileCarousel from '../../presentational/design/MobileCarousel';
+
+import { Responsive } from 'semantic-ui-react'
 
 class Result extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      windowHeight: window.innerHeight
     };
   }
+
   render() {
     return (
       <div>
-        <CommCarousel />
+        <Responsive minWidth={900}>
+          <CommCarousel />
+        </Responsive>
+        <Responsive maxWidth={899}>
+          <MobileCarousel windowHeight={this.state.windowHeight}/>
+        </Responsive>
       </div>
     );
   }
+
+   componentDidMount() {
+     window.onresize = () => {
+       this.setState({
+         windowHeight: window.innerHeight
+       })
+     }
+   }
 }
 
 export default Result;
