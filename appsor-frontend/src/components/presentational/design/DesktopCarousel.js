f480b010ee759e48f7b57240dfc36cf32ee5382c
Jonathan Cancio <jonahcancio@gmail.com>
2018-08-30 15:53:00 +0800

    Fixed desktop version scroll and height

--- appsor-frontend/src/components/presentational/design/DesktopCarousel.js
+++ appsor-frontend/src/components/presentational/design/DesktopCarousel.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Slider from "react-slick";
+import React, { Component } from 'react';
+import Slider, {slickNext} from "react-slick";
 import "../../../assets/stylesheets/DesktopCarousel.css";
 import { Button } from 'semantic-ui-react'
 
@@ -29,39 +29,62 @@ function SampleNextArrow(props) {
   );
 }
 
-function CommCarousel(props) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    className: 'carousel-main',
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-    dotsClass: 'slick-dots carousel-dots',
+class CommCarousel extends Component {
+  constructor(props) {
+    super(props)
+    this.settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      className: 'carousel-main',
+      dotsClass: 'slick-dots carousel-dots',
+      variableHeight: true,
+    }
+
+    this.slideScroll = this.slideScroll.bind(this);
   }
 
-  return (
-		<div className="carousel-container">
-			<Slider {...settings}>
-        {CommArray.map((comm, index) => (
-          <div className="carousel-div" key={index}>
-            <img src={comm.img} alt='engg-comm'/>
-            <div className="slide-title">
-              <p>{comm.name}<br/>COMMITTEE</p>
-              <ReadMeModal
-                name={comm.name}
-                key={index}
-                trigger={<Button>Read More</Button>}
-              />
-            </div>
-          </div>
-        ))}
-      </Slider>
-		</div>
-  );
+  slideScroll(y) {
+    y < 0? (
+      this.slider.slickNext()
+    ) : (
+      this.slider.slickPrev()
+    )
+  }
+
+  componentWillMount() {
+    window.addEventListener('wheel', (e) => {
+      this.slideScroll(e.wheelDelta);
+    })
+  }
+
+  render() {
+    return (
+    <div onScroll={slickNext} className="carousel-container">
+      <Slider
+        ref={slider => this.slider = slider}
+        {...this.settings}
+      >
+         {CommArray.map((comm, index) => (
+           <div className="carousel-div" key={index}>
+             <img src={comm.img} alt='engg-comm'/>
+             <div className="slide-title">
+               <p>{comm.name}<br/>COMMITTEE</p>
+               <ReadMeModal
+                 name={comm.name}
+                 key={index}
+                 trigger={<Button>Read More</Button>}
+               />
+             </div>
+           </div>
+         ))}
+       </Slider>
+     </div>
+    );
+  }
 }
 
 export default CommCarousel;
