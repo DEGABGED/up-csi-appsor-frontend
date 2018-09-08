37edc472e9ecc12f6df63a8ee032c7808dcbb6f4
Jonathan Cancio <jonahcancio@gmail.com>
2018-09-08 22:07:25 +0800

    Added autoscroll; Made mobile modal fullscreen

--- appsor-frontend/src/components/presentational/design/ReadMeModal.js
+++ appsor-frontend/src/components/presentational/design/ReadMeModal.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Modal, Header } from 'semantic-ui-react'
-import "../../../assets/stylesheets/ReadMeModal.css";
+import { Modal, Header } from 'semantic-ui-react';
+import '../../../assets/stylesheets/ReadMeModal.css';
 
-function camelizeCase(string) {
-		return string.toLowerCase().replace(/\b\w/g, c => c.toUpperCase())
-}
+class ReadMeModal extends Component {
 
-function ReadMeModal(props) {
-	return (
-		<Modal trigger={props.trigger}
-			dimmer={'blurring'}
-			className="main-modal"
-		>
-			<Modal.Header className="modal-img" style={{backgroundImage: "url(" + props.img + ")"}}>
-			</Modal.Header>
-			<Modal.Content className="modal-content">
-				<Header className="modal-header">CHECKOUT THE {props.name} COMMITTEE</Header>
-				<Modal.Description>
-					<p>{props.description}</p>
-				</Modal.Description>
-			</Modal.Content>
-		</Modal>
-	)
+  camelizeCase(string) {
+    return string.toLowerCase().replace(/\b\w/g, c => c.toUpperCase());
+  }
+
+  render() {
+    const props = this.props;
+    return (
+      <Modal
+        trigger={props.trigger}
+        dimmer="blurring"
+				className="main-modal"
+				closeIcon
+        onOpen={props.pauseCarousel}
+        onClose={props.playCarousel}
+			>
+        <Modal.Header className="modal-img" style={{ backgroundImage: `url(${props.img})` }} />
+        <Modal.Content className="modal-content">
+          <Header className="modal-header">CHECKOUT THE {props.name} COMMITTEE</Header>
+          <Modal.Description>
+            <p>{props.description}</p>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
+    );
+  }
+
 }
 
 ReadMeModal.propTypes = {
   name: PropTypes.string,
-	description: PropTypes.string,
-	img: PropTypes.string,
+  description: PropTypes.string,
+  img: PropTypes.string,
+  pauseCarousel: PropTypes.func,
+  playCarousel: PropTypes.func,
 };
 
 ReadMeModal.defaultProps = {
-  name: "UNKNOWN",
-	description: "No description found"
+  name: 'UNKNOWN',
+  description: 'No description found',
 };
 
 export default ReadMeModal;
