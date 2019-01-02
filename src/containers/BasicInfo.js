8f71c9cc3d64d77a02e9b29bfc0807142a6e085d
Francis dela Cruz <franciszac.dlc@gmail.com>
2019-01-02 15:52:03 +0800

    Remove BasicInfoView to flatten Basic Info path

--- src/containers/BasicInfo.js
+++ src/containers/BasicInfo.js
@@ -1,34 +1,109 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import BasicInfoView from '../components/BasicInfoView';
+import { Container } from 'semantic-ui-react';
 
-class BasicInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
+import FormTextField from '../components/FormTextField';
+import FormMaskedField from '../components/FormMaskedField';
 
-  handleChange(event) {
-    const { name, value } = event.currentTarget;
-    const newBasicInfo = { ...this.props.basicInfo, [name]: value };
-    newBasicInfo[name] = value;
-    this.props.handleChange(newBasicInfo);
-  }
+function BasicInfo(props) {
+  const contactNoFormat = '(+63) 9';
+  const studentNoFormat = '20  -';
+  const defaultDate = '1999-01-01';
 
-  render() {
-    return (
-      <BasicInfoView
-        basicInfo={this.props.basicInfo}
-        handleChange={this.handleChange}
-        errors={this.props.errors}
-      />
-    );
-  }
-}
+  return (
+    <Container className="basic-info-container">
+      <div className="row">
+        <FormTextField
+          label="Last Name"
+          name="lastName"
+          values={props.basicInfo}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="last-name"
+        />
+        <FormTextField
+          label="M. I."
+          name="middleInitial"
+          values={props.basicInfo}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="middle-initial"
+        />
+        <FormTextField
+          label="First Name"
+          name="firstName"
+          values={props.basicInfo}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="first-name"
+        />
+      </div>
+      <div className="row">
+        <FormTextField
+          label="Nickname"
+          name="nickname"
+          values={props.basicInfo}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="nickname"
+        />
+        <FormMaskedField
+          label="Student Number"
+          name="studentNumber"
+          values={props.basicInfo}
+          defaultValue={studentNoFormat}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="student-number"
+        />
+        <FormTextField
+          label="Birthday"
+          name="birthday"
+          type="date"
+          values={props.basicInfo}
+          defaultValue={defaultDate}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="birthday"
+        />
+      </div>
+      <div className="row">
+        <FormTextField
+          label="Email"
+          name="email"
+          values={props.basicInfo}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="email"
+        />
+        <FormMaskedField
+          label="Contact Number"
+          name="contactNumber"
+          values={props.basicInfo}
+          defaultValue={contactNoFormat}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="contact-number"
+        />
+      </div>
+      <div className="row">
+        <FormTextField
+          label="Address"
+          name="address"
+          values={props.basicInfo}
+          onChange={props.handleChange}
+          errors={props.errors}
+          className="address"
+        />
+      </div>
+    </Container>
+  );
+};
+
 
 BasicInfo.propTypes = {
-  handleChange: PropTypes.func.isRequired,
   basicInfo: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
   errors: PropTypes.object,
 };
 
--- src/containers/PersonalInfo.js
+++ src/containers/PersonalInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from 'semantic-ui-react';
 
@@ -9,35 +9,51 @@ import GlitterPics from '../components/design/GlitterPics';
 import '../assets/stylesheets/Base.css';
 import '../assets/stylesheets/PersonalInfo.css';
 
-const PersonalInfo = props => (
-  <Container id={props.id} textAlign="center" className="page-container personal-info-container">
-    <GlitterPics
-      begin={5}
-      end={10}
-      columns={2}
-      offset={0}
-    />
-    <div className="personal-info-form">
-      <h1 className="page-title">Personal Information</h1>
-      <BasicInfo
-        handleChange={props.handleChangeBasicInfo}
-        basicInfo={props.basicInfo}
-        errors={props.errorsBasicInfo}
-      />
-      <SkillsInterests
-        handleChange={props.handleChangeSkillsInterests}
-        skillsInterests={props.skillsInterests}
-        errors={props.errorsSkillsInterests}
-      />
-    </div>
-    <GlitterPics
-      begin={0}
-      end={5}
-      columns={2}
-      offset={1}
-    />
-  </Container>
-);
+class PersonalInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChangeBasicInfo = this.handleChangeBasicInfo.bind(this);
+  }
+
+  handleChangeBasicInfo(event) {
+    const { name, value } = event.currentTarget;
+    const newBasicInfo = { ...this.props.basicInfo, [name]: value };
+    newBasicInfo[name] = value;
+    this.props.handleChangeBasicInfo(newBasicInfo);
+  }
+
+  render() {
+    return (
+      <Container id={this.props.id} textAlign="center" className="page-container personal-info-container">
+        <GlitterPics
+          begin={5}
+          end={10}
+          columns={2}
+          offset={0}
+        />
+        <div className="personal-info-form">
+          <h1 className="page-title">Personal Information</h1>
+          <BasicInfo
+            handleChange={this.handleChangeBasicInfo}
+            basicInfo={this.props.basicInfo}
+            errors={this.props.errorsBasicInfo}
+          />
+          <SkillsInterests
+            handleChange={this.props.handleChangeSkillsInterests}
+            skillsInterests={this.props.skillsInterests}
+            errors={this.props.errorsSkillsInterests}
+          />
+        </div>
+        <GlitterPics
+          begin={0}
+          end={5}
+          columns={2}
+          offset={1}
+        />
+      </Container>
+    );
+  }
+}
 
 PersonalInfo.propTypes = {
   id: PropTypes.string,
