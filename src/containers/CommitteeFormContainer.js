6b9ff4801291d756ddf1db264a779a2f375f9419
Francis dela Cruz <franciszac.dlc@gmail.com>
2019-01-14 16:24:50 +0800

    Optimize Committee Container loading

--- src/containers/CommitteeFormContainer.js
+++ src/containers/CommitteeFormContainer.js
@@ -1,39 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ScrollAnimation from 'react-animate-on-scroll';
 import { Container, Button } from 'semantic-ui-react';
 
 import CommitteeFormView from '../components/CommitteeFormView';
 import { committeeOptions } from '../helpers/defaultOptions';
+import { shallowEqual } from '../helpers/generics';
 import '../assets/stylesheets/Base.css';
 import '../assets/stylesheets/Committee.css';
 
-function Committee(props) {
-  // To avoid repeating code, this iterates through all the Froms
-  const forms = [];
+class Committee extends Component {
+  shouldComponentUpdate(nextProps) {
+    // Check if the list of objects are equal
+    const prev = this.props.committees;
+    const next = nextProps.committees;
+    let areEqual = prev.length === next.length;
 
-  for (let id = 0; id < 3; id++) {
-    forms.push(<CommitteeFormView
-      key={id}
-      formID={id}
-      options={committeeOptions}
-      value={props.committees[id]}
-      onChangeCommittee={props.onChangeCommittee}
-      onChangeReason={props.onChangeReason}
-      errors={props.errors !== undefined ? props.errors[id] : undefined}
-      duplicates={props.duplicates !== undefined ? props.duplicates[id] : undefined}
-    />);
+    areEqual = areEqual && prev
+      .map((element, index) => shallowEqual(element, next[index]))
+      .reduce((acc, element) => acc && element, true);
+
+    return !areEqual;
   }
 
-  return (
-    <ScrollAnimation animateIn="fadeIn" animateOut="fadeOutLeft" duration={0.5}>
-      <Container textAlign="center" className="page-container committee-container" id={props.id} >
-        <h1 className="page-title">Committee Preference</h1>
-        { forms }
-        <Button primary type="submit" disabled={props.isSubmitting} >Submit</Button>
-      </Container>
-    </ScrollAnimation>
-  );
+  render() {
+    // To avoid repeating code, this iterates through all the Froms
+    const forms = [];
+
+    for (let id = 0; id < 3; id++) {
+      forms.push(<CommitteeFormView
+        key={id}
+        formID={id}
+        options={committeeOptions}
+        value={this.props.committees[id]}
+        onChangeCommittee={this.props.onChangeCommittee}
+        onChangeReason={this.props.onChangeReason}
+        errors={this.props.errors !== undefined ? this.props.errors[id] : undefined}
+        duplicates={this.props.duplicates !== undefined ? this.props.duplicates[id] : undefined}
+      />);
+    }
+
+    return (
+      <ScrollAnimation animateIn="fadeIn" animateOut="fadeOutLeft" duration={0.5}>
+        <Container textAlign="center" className="page-container committee-container" id={this.props.id} >
+          <h1 className="page-title">Committee Preference</h1>
+          { forms }
+          <Button primary type="submit" disabled={this.props.isSubmitting} >Submit</Button>
+        </Container>
+      </ScrollAnimation>
+    );
+  }
 }
 
 Committee.propTypes = {
