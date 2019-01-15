5276512b0a370258cc4926852d6967866a72f43e
Francis dela Cruz <franciszac.dlc@gmail.com>
2019-01-15 10:58:44 +0800

    Turn FormDropdown into a Class component and controlled input

--- src/components/FormDropdown.js
+++ src/components/FormDropdown.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Dropdown } from 'semantic-ui-react';
 
-function FormDropdown(props) {
-  return (
-    <Dropdown
-      {...props}
-      selection
-      search
-      onChange={(event, { value }) => {
-        let field;
-        if (event.keyCode === 13) { /* Enter */
-          field = event.target.parentNode.attributes.name.value;
-        } else {
-          try {
-            field = event.currentTarget.parentNode.parentNode.attributes.name.value;
-          } catch (error) {
+class FormDropdown extends Component {
+  shouldComponentUpdate() {
+    return true;
+  }
+
+  render() {
+    return (
+      <Dropdown
+        {...this.props}
+        selection
+        search
+        onChange={(event, { value }) => {
+          let field;
+          if (event.keyCode === 13) { /* Enter */
             field = event.target.parentNode.attributes.name.value;
+          } else {
+            try {
+              field = event.currentTarget.parentNode.parentNode.attributes.name.value;
+            } catch (error) {
+              field = event.target.parentNode.attributes.name.value;
+            }
           }
-        }
-        props.onChange(field, value);
-      }}
-    />
-  );
+          this.props.onChange(field, value);
+        }}
+      />
+    );
+  }
 }
 
 FormDropdown.propTypes = {
