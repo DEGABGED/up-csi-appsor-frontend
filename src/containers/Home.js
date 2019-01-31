fd1bc87bf8a8877eb0d77aba1958f70518917576
Adel Mandanas <mandanas.adel@gmail.com>
2019-01-31 12:58:35 +0800

    Add committee choice assets and fonts

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Container, Image, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
-import upcsi from '../assets/images/upcsi.png';
+import upcsi from '../assets/images/upcsi_white.png';
 import '../assets/stylesheets/Home.css';
 
 const Home = () => (
   <Container className="home-container" textAlign="center">
     <Image className="animated fadeInUp" src={upcsi} size="mini" centered />
     <div className="animated bounceInUp delay-5s">
-      <h1 className="sub-title">TAKE A SIP AT</h1>
-      <h1 className="main-title">Innov Cafe</h1>
+      <h1 className="sub-title">TAKE A LOOK AT THE</h1>
+      <h1 className="main-title">KALEIDOSCOPE</h1>
     </div>
     <Link className="animated fadeInUp chevron-button" to="/committees">
       <Icon name="chevron circle right" link size="huge" />
